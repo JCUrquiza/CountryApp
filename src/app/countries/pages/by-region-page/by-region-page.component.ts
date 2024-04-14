2e7f04dd644824e0b3b77a399a400a15ea768093
Juan <gurquiza8@gmail.com>
2024-04-14 14:01:16 -0500

    implementing ngClass for region selected

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Capital } from '../../interfaces/capitals';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,10 +12,14 @@ import { Capital } from '../../interfaces/capitals';
 export class ByRegionPageComponent {
 
   public countries: Capital[] = [];
+  public regions:  Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor( private countriesService: CountriesService ) {}
 
-  searchByRegion(region: string): void {
+  searchByRegion(region: Region): void {
+
+    this.selectedRegion = region;
 
     this.countriesService.searchRegion(region)
       .subscribe( countries => {
